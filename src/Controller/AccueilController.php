7c7c1236c10e9446d79f2657fb334429c097e076
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2020-11-03 23:00:04 +0000

    Integration du template

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_ccueil")
+     */
+    public function index(): Response
+    {
+        return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+}
