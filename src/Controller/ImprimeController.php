0035793e9d0476038303311f5905f7aa0e93aa6d
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2021-01-04 21:24:36 +0000

    DOMPDF Relance

--- src/Controller/ImprimeController.php
+++ src/Controller/ImprimeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Classe;
 use App\Entity\Eleve;
 use App\Entity\Inscription;
+use App\Entity\Scolarite;
 use App\Entity\Versement;
 use App\Utilities\GestionEleve;
 use App\Utilities\GestionImpression;
@@ -121,6 +123,76 @@ class ImprimeController extends AbstractController
 
         // On génère le nom du fichier
         $fichier = 'Inscription-recu-N°:'.$inscription->getNumero().'.pdf';
+
+        // On envoie le PDF au navigateur
+        $dompdf->stream($fichier,[
+            'Attachement' => false
+        ]);
+
+        return new Response();
+    }
+
+    /**
+     * @Route("/convocation/{classe}/1", name="imprime_convocation_classe", methods={"GET","POST"})
+     */
+    public function convocation($classe)
+    {
+        $annee = $this->gestionEleve->annee();
+        $class = $this->em->getRepository(Classe::class)->findOneBy(['libelle'=>$classe]); //dd($class->getId());
+        $scolarites = $this->em->getRepository(Scolarite::class)->findByAnneeAndClasse($annee, $classe);
+
+        $mois_encours = date('m');
+        $annee_encours = date('Y');
+        $jour_encours = date('d');
+
+        if ($jour_encours >= 05){
+            if ($mois_encours === 12) $mois = 01;
+            else $mois = $mois_encours + 1;
+
+            $date_echeance = '05/'.$mois.'/'.$annee_encours;
+            $date_encours = $jour_encours.'/'.$mois_encours.'/'.$annee_encours;
+        }else{
+            if ($mois_encours === 12) $mois = 01;
+            else $mois = $mois_encours;
+
+            $date_echeance = '05/'.$mois.'/'.$annee_encours;
+            $date_encours = $jour_encours.'/'.$mois_encours.'/'.$annee_encours;
+        }
+
+        // O definit les options du PDF
+        $pdfOptions = new Options();
+
+        // Definiton de la police par defaut
+        $pdfOptions->set('defaultFont', 'Arial');
+        $pdfOptions->setIsRemoteEnabled(true);
+        $pdfOptions->setIsHtml5ParserEnabled(true);
+
+        // On instancie Dompdf
+        $dompdf = new Dompdf($pdfOptions);
+        $context = stream_context_create([
+            'ssl' => [
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            ]
+        ]);
+        $dompdf->setHttpContext($context);
+
+        //dd($scolarites);
+        $html = $this->render('imprime/convocation.html.twig',[
+            'scolarites' => $scolarites,
+            'annee' => $annee,
+            'classe' => $class,
+            'date_echeance' => $date_echeance,
+            'date_jour' => $date_encours
+        ]);
+
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+
+        // On génère le nom du fichier
+        $fichier = 'Relance-'.$classe.'-du_'.$annee_encours.'-'.$mois_encours.'-'.$jour_encours.'.pdf';
 
         // On envoie le PDF au navigateur
         $dompdf->stream($fichier,[
